3628bb4d369b65b2fe3dc50eec34f61068056f7e
Pasquale De Martino <pdemartino@expedia.com>
2015-03-27 09:50:37 +0100

    bk-profile: command to reload the bash settings, useful after modifications

--- plugins/commands.sh
+++ plugins/commands.sh
@@ -3,3 +3,6 @@ alias bk-edit-host="mkdir -p $BK_HOST_SETTINGS && vim $BK_HOST_SETTINGS"
 
 # Open vim and edit common settigs
 alias bk-edit-common="mkdir -p $BK_USER_COMMON_SETTINGS && vim $BK_USER_COMMON_SETTINGS"
+
+# Reload settings after modification
+alias bk-profile="source $HOME/.bashrc"
