957def84002c18f1227f79ed2e8b439e627ccb89
Pasquale De Martino <pdemartino@expedia.com>
2016-10-30 09:57:12 +0100

    Changes bk-edit

--- plugins/commands.sh
+++ plugins/commands.sh
@@ -1,8 +1,8 @@
 # Open vim and edit host settings
-alias bk-edit-host="mkdir -p $BK_HOST_SETTINGS && vim $BK_HOST_SETTINGS"
+alias bk-edit-host="mkdir -p $BK_HOST_SETTINGS && $EDITOR $BK_HOST_SETTINGS"
 
 # Open vim and edit common settigs
-alias bk-edit-common="mkdir -p $BK_USER_COMMON_SETTINGS && vim $BK_USER_COMMON_SETTINGS"
+alias bk-edit-common="mkdir -p $BK_USER_COMMON_SETTINGS && $EDITOR $BK_USER_COMMON_SETTINGS"
 
 # Reload settings after modification
 alias bk-profile="source $HOME/.bashrc"
